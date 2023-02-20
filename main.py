e2947c286daef3d50f0e6a1ffe0d900893f0d246
swim_raspi <swim@surromind.ai>
2023-02-20 18:59:40 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import time
+import pytesseract
+from PIL import Image
+import cv2
+
+img = cv2.imread('WS220ah9044.png',cv2.IMREAD_COLOR) #Open the image from which charectors has to be recognized
+img = cv2.resize(img, (620,480) ) #resize the image if required
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert to grey to reduce detials
+gray = cv2.bilateralFilter(gray, 11, 17, 17) #Blur to reduce noise
+
+start = time.time()
+original = pytesseract.image_to_string(gray, config='')
+#test = (pytesseract.image_to_data(gray, lang=None, config='', nice=0) ) #get confidence level if required
+#print(pytesseract.image_to_boxes(gray))
+
+print(f"processing time: {time.time() - start}")
+print (original)
